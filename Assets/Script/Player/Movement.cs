8bea459cd69065ff7b4c44ecbb89a607cfc43e57
phuckkdq9 <phucpdh@bbold.vn>
2023-11-05 02:41:32 +0700

    fix Anim p2

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -10,7 +10,7 @@ public class Movement : Darwin
     [SerializeField] protected PlayerCtrl playerCtrl;           // chứa coponent quản lí các component khác
     private Vector3 moveDir;
     public Vector3 MoveDir { get => moveDir; set => moveDir = value; }
- 
+    [SerializeField] protected Animator animator;
 
     protected override void LoadComponent()
     {
@@ -26,58 +26,45 @@ public class Movement : Darwin
 
     void Update()
     {
-        if(InputManager.Instance.IsAttack) return;
         this.Moving();          // gọi hàm di chuyển
-        this.AnimationRun();    // gọi hàm animation chạy
+        Jumping();
     }
 
     // hàm di chuyển
     protected virtual void Moving()
     {
-        if(InputManager.Instance.MoveLeft ){
-            if(is_ground){
-                moveDir = Vector3.left;
-                transform.parent.position += moveDir * moveSpeed * Time.deltaTime ;
-            }
-            else{
-                moveDir = Vector3.left;
-                transform.parent.position += moveDir * 5 * Time.deltaTime ;
-            }    
-            transform.parent.localScale = new Vector3(-1, transform.parent.localScale.y, transform.parent.localScale.z);
-        }
-        else if (InputManager.Instance.MoveRight){
-            if(is_ground){
-                moveDir = Vector3.right;
-                transform.parent.position += moveDir * moveSpeed * Time.deltaTime ;
-            }
-            else{
-                moveDir = Vector3.right;
-                transform.parent.position += moveDir * 5 * Time.deltaTime ;
-            }      
-            transform.parent.localScale = new Vector3(1, transform.parent.localScale.y, transform.parent.localScale.z);
-        }
-        if (InputManager.Instance.IsJump && this.is_ground){
+        float xDir = Input.GetAxisRaw("Horizontal");
+        if(is_ground)playerCtrl.Mvrigidbody2D.velocity = new Vector2(moveSpeed * xDir , playerCtrl.Mvrigidbody2D.velocity.y);
+        else playerCtrl.Mvrigidbody2D.velocity = new Vector2(moveSpeed/2 * xDir , playerCtrl.Mvrigidbody2D.velocity.y);
+        if(playerCtrl.Mvrigidbody2D.velocity.x > 0)
+        {
+            transform.parent.localScale = new Vector3(1,1,1);
+            moveDir = Vector3.right;
+        } 
+        if(playerCtrl.Mvrigidbody2D.velocity.x < 0)
+        {
+            transform.parent.localScale = new Vector3(-1,1,1);
+            moveDir = Vector3.left;
+        } 
+        if(is_ground)   animator.SetFloat("Running", Mathf.Abs(xDir));
+    }
+
+    protected virtual void Jumping()
+    {
+        if (InputManager.Instance.IsJump && this.is_ground)
+        {
             playerCtrl.Mvrigidbody2D.AddForce(Vector2.up* forceJump);
-            // rb.velocity = Vector2.up * forceJump;
+            animator.SetBool("Jump", true);
             this.is_ground = false; 
         }
+        animator.SetFloat("Yveloc", playerCtrl.Mvrigidbody2D.velocity.y);
+
     }
 
     private void OnCollisionEnter2D(Collision2D col){
         if(col.gameObject.CompareTag("Ground")){
             this.is_ground = true;
+            animator.SetBool("Jump", !is_ground);
         }  
-    }
-
-    protected virtual void AnimationRun()
-    {
-        if((InputManager.Instance.MoveLeft && is_ground) || (InputManager.Instance.MoveRight && is_ground))
-        {
-            playerCtrl.Animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            playerCtrl.Animator.SetBool("isRunning", false);
-        }
     }
 }
